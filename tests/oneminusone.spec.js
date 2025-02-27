78794968648293f77fe403be56a236436d55a305
iker99 <iker200409@gmail.com>
2025-02-27 12:58:46 -0600

    MinusTests

--- tests/oneminusone.spec.js
+++ tests/oneminusone.spec.js
@@ -2,24 +2,24 @@
 const { Builder, By, Key, until } = require('selenium-webdriver')
 const assert = require('assert')
 
-describe('Test1', function() {
+describe('One minus one', function() {
   this.timeout(30000)
   let driver
   let vars
   beforeEach(async function() {
-    driver = await new Builder().forBrowser('edge').build()
+    driver = await new Builder().forBrowser('chrome').build()
     vars = {}
   })
   afterEach(async function() {
     await driver.quit();
   })
-  it('Test1', async function() {
+  it('One minus one', async function() {
     await driver.get("http://localhost:8000/src/")
-    await driver.manage().window().setRect(1061, 812)
+    await driver.manage().window().setRect(1552, 832)
     await driver.findElement(By.id("num1")).click()
     await driver.findElement(By.id("num1")).sendKeys("1")
     await driver.findElement(By.id("num2")).click()
     await driver.findElement(By.id("num2")).sendKeys("1")
-    await driver.findElement(By.css("button:nth-child(1)")).click()
+    await driver.findElement(By.css("button:nth-child(2)")).click()
   })
 })
